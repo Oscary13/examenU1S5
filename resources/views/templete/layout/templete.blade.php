e5882b3cdd18b13252068e1bef7f0d5274708c65
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2022-02-03 03:23:32 -0600

    edits2

--- resources/views/templete/layout/templete.blade.php
+++ resources/views/templete/layout/templete.blade.php
@@ -80,17 +80,17 @@
                     <form method="post" action="#">
                         <div class="fields">
                             <div class="field half">
-                                <input type="text" name="name" id="name" placeholder="Name" />
+                                <input type="text" name="name" id="name" placeholder="Nombre" />
                             </div>
                             <div class="field half">
-                                <input type="email" name="email" id="email" placeholder="Email" />
+                                <input type="email" name="email" id="email" placeholder="Correo electrónico" />
                             </div>
                             <div class="field">
-                                <textarea name="message" id="message" placeholder="Message"></textarea>
+                                <textarea name="message" id="message" placeholder="Mensaje"></textarea>
                             </div>
                         </div>
                         <ul class="actions">
-                            <li><input type="submit" value="Send" class="primary" /></li>
+                            <li><input type="submit" value="Enviar" class="primary" /></li>
                         </ul>
                     </form>
                 </section>
